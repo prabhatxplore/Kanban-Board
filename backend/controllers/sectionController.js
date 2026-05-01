c3826c1b82d6b0a4ad3f6391c7380c326f2785b2
Prabhat BK <prabhatbk4@gmail.com>
2026-05-01 07:59:25 +0545

    add: section: create controller functionality

--- backend/controllers/sectionController.js
+++ backend/controllers/sectionController.js
@@ -1,1 +1,23 @@
-exports.createSectionController = (req, res) => {};
+const Section = require("../models/Section");
+
+exports.createSectionController = async (req, res) => {
+  try {
+    const { title, boardId, order } = req.body;
+    const section = new Section({ title, boardId, order });
+
+    await section.save();
+
+    res.status(500).json({
+      success: true,
+      message: "Successfully added column",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+exports.reorderSectionController = (req, res) => {
+};
+exports.removeSectionController = (req, res) => {};
